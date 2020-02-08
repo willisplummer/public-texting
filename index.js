1d3879a9882658aaef9983c7e766d5763ddfcaab
willis plummer <willisplummer@gmail.com>
2020-02-08 13:30:20 -0500

    fix queries etc

--- index.js
+++ index.js
@@ -18,35 +18,72 @@ app.use(bodyParser.json());
 const users = { [willisNumber]: 'Willis', [luizaNumber]: 'Luiza' }
 const chats = []
 
-app.post('/messages', (req, res) => {
+const getUserByPhoneNumber = (phoneNumber) =>
+  pool.query(
+    "SELECT * FROM users WHERE phone_number = $1",
+    [phoneNumber])
+    .then(results => results.rows[0])
+
+const getConversationByParticipantId = (userId) =>
+  pool.query(
+    ```
+    SELECT * FROM conversations
+    WHERE first_user_id = 1
+      OR second_user_id = 1
+    ```,
+    [userId]
+  ).then(results => results.rows[0])
+
+const writeMessage = (fromUser, conversation, msgBody) =>
+  pool.query(
+    "INSERT INTO messages (conversation_id, sender_id, body) VALUES ($1, $2, $3)",
+    [conversation.id, fromUser.id, msgBody]
+  )
+
+const getMessages = (conversationId) =>
+  pool.query(
+    'SELECT * FROM messages WHERE conversation_id = $1',
+    [conversationId]
+  ).then(results => results.rows)
+
+app.post('/messages', async (req, res) => {
   console.log(req.body);
 
   const fromNumber = req.body.From
-  const fromUser = users[fromNumber]
+  const fromUser = await getUserByPhoneNumber(fromNumber)
 
+  console.log(fromNumber, fromUser)
   // if there's no fromUser, we just throw it away
   if (fromUser) {
     const msgBody = req.body.Body
 
     // unsafe
+    const conversation = await getConversationByParticipantId(fromUser.id)
+    console.log(conversation)
+
+    // TODO
     const proxyToNumber = Object.keys(users).filter(x => x != fromNumber)[0]
 
     // write to memory
+    await writeMessage(fromUser, conversation, msgBody)
     chats.push({ from: fromUser, body: msgBody })
 
     // proxy number to other conversation participant
     twilioClient.messages.create({
       body: msgBody,
       to: proxyToNumber,
-      from: twilioNumber
+      from: conversation.twilio_number
     })
   }
 
   res.send('<Response></Response>');
 })
 
-app.get('/messages', (req, res) => {
-  res.send(JSON.stringify(chats))
+app.get('/messages/:conversationId', async (req, res) => {
+  const conversationId = req.params.conversationId
+  const messages = await getMessages(conversationId)
+  console.log(messages)
+  res.send(JSON.stringify(messages))
 })
 
 // TODO: remove before publishing (just for testing locally)
