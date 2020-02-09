e801cd5da6f87586597eb7b7d046edef5409a96e
willis plummer <willisplummer@gmail.com>
2020-02-09 13:15:01 -0500

    typo ||

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bodyParser = require("body-parser");
 // const twilioClient = require('twilio')(accountSid, authToken);
 
 const app = express()
-const port = process.env.PORT | 3000
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
