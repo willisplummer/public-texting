141675b49e18afa53586cee489a565535af9e641
willis plummer <willisplummer@gmail.com>
2020-02-12 23:51:32 -0500

    remove unnecessary local sans pg instructions

--- config.js
+++ config.js
@@ -8,20 +8,9 @@ const { Pool } = require('pg')
 
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
-// make it easier to run locally without a database
-let pool
-if (process.env.DATABASE_URL || (process.env.DB_USER && process.env.DB_PASSWORD && process.env.DB_PORT && process.env.DB_DATABASE)) {
-  pool = new Pool({
-    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-    ssl: isProduction,
-  })
-} else {
-  console.log('running with mock database - provide necessary env variables to run with real DB')
-  const query = () => Promise.resolve({ rows: [] })
-  pool = {
-    connect: () => ({ query }),
-    query
-  }
-}
+const pool = new Pool({
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: isProduction,
+})
 
 module.exports = { pool }
